49b795982ef28a5ce9068b4267682bae31d9f965
Funky <nilo8370@gmail.com>
2023-11-24 09:52:29 +0700

    added new project

---
+++ caffeApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace caffeApp.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ caffeApp/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+﻿using Avalonia.Controls;
+
+namespace caffeApp.Views;
+
+public partial class MainView : UserControl
+{
+    public MainView()
+    {
+        InitializeComponent();
+    }
+}
